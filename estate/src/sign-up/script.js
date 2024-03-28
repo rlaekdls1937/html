5118f996d400113a89a85e309bc43d434c3151d9
Kim_dain <ekdls15937@naver.com>
2024-03-28 12:18:51 +0900

    2024.03.28 12:18

--- estate/src/sign-up/script.js
+++ estate/src/sign-up/script.js
@@ -4,11 +4,11 @@ const EMAIL = 'email@email.com';
 const AUTH_NUMBER = '1010';
 
 let id = '', password = '', passwordCheck = '', email = '', authNumber = '';
-let isDuplicate = true, isEmail = false, isDuplicateEmail = true, isEqualAuthNumber = false;
+let isDuplicate = true, isPasswordPattern = false, isEqualPassword = false, isEmail = false, isDuplicateEmail = true, isEqualAuthNumber = false;
 
 const idInputElement = document.getElementById('id');
 const passwordInputElement = document.getElementById('password');
-const passwordCheckInputElement = document.getElementById('password-check')
+const passwordCheckInputElement = document.getElementById('password-check');
 const emailInputElement = document.getElementById('email');
 const authNumberInputElement = document.getElementById('auth-number');
 
@@ -17,6 +17,8 @@ const checkEmailButtonElement = document.getElementById('check-email-button');
 const checkAuthNumberButtonElement = document.getElementById('check-auth-number-button');
 
 const idMessageElement = document.getElementById('id-message');
+const passwordMessageElement = document.getElementById('password-message');
+const passwordCheckMessageElement = document.getElementById('password-check-message');
 const emailMessageElement = document.getElementById('email-message');
 const authNumberMessageElement = document.getElementById("auth-number-message" );
 
@@ -32,13 +34,41 @@ function onIdInputHandler (event){
 }
 
 function onPasswordInputHandler (event) {
+    // 비밀번호 변수에 이벤트가 발생한 실제 요소의 value 값을 할당
     password = event.target.value;
+    // 비밀번호 패턴을 정규식으로 영문자와 숫자를 반드시 포함한 상태로 8~13자로 지정
+    const passwordReg = /^(?=.*[a-zA-Z])(?=.*[0-9])[a-zA-Z0-9]{8,13}$/;
+    // 비밀번호 변수에 들어있는 값이 비밀번호 패턴과 일치하는지 확인
+    isPasswordPattern = passwordReg.test(password);
+    // 비밀번호 패턴이 일치하지 않을 때
+    if (!isPasswordPattern){
+    // passwordMessage 요소의 클래스명을 'input-message error로 바꾼다
+    passwordMessageElement.className = 'input-message error';
+    // passwordMessage 요소의 텍스트를 '영문 숫자를 혼용하여 8 ~ 13자 입력해주세요'로 바꾼다
+    passwordMessageElement.textContent = '영문, 숫자를 혼용하여 8~13자 입력해주세요';
+    // 변경 작업이 이뤄진 후 함수를 종료한다
+    return;
+    }
+    // 비밀번호 패턴이 일치할 경우에만 아래 코드가 실행됨
+    
+    // passwordMessage 요소의 클래스명을 'input-message'로 바꾼다
+    passwordMessageElement.className = 'input-message';
+    // passwordMessage 요소의 텍스트를 빈문자열로 바꾼다.
+    passwordMessageElement.textContent = '';
 }
 
 function onPasswordCheckInputHandler (event){
     passwordCheck = event.target.value;
-}
 
+    isEqualPassword = password === passwordCheck;
+    if (!isEqualPassword) {
+        passwordCheckMessageElement.className = 'input-message error';
+        passwordCheckMessageElement.textContent = '비밀번호가 일치하지 않습니다.';
+        return;
+    }
+    passwordCheckMessageElement.className = 'input-message';
+    passwordCheckMessageElement.textContent = '';
+}
 
 function onEmailInputHandler (event){
     email = event.target.value;
@@ -48,12 +78,11 @@ function onEmailInputHandler (event){
     if (email) checkEmailButtonElement.className = 'input-primary-button'; 
     else checkEmailButtonElement.className = 'input-disable-button';
 
-
 }
 
 function onAuthNumberInputHandler (event){
     authNumber = event.target.value;
-    isEqualAuthNumber - false;
+    isEqualAuthNumber = false;
 
     if (authNumber) checkAuthNumberButtonElement.className = 'input-primary-button'; 
     else checkAuthNumberButtonElement.className = 'input-disable-button';
@@ -93,8 +122,8 @@ function onCheckDuplicateClickHandler (event){
         idMessageElement.textContent = '이미 사용중인 아이디 입니다.';
         return;
     }
-        idMessageElement.className = 'input-message primary';
-        idMessageElement.textContent = '사용 가능한 아이디 입니다.';
+    idMessageElement.className = 'input-message primary';
+    idMessageElement.textContent = '사용 가능한 아이디 입니다.';
     
 }
 
@@ -109,7 +138,7 @@ function onCheckEmailClickHandler (event){
         return;
     }
 
-    const isDuplicateEmail = email === EMAIL;
+    isDuplicateEmail = email === EMAIL;
     if (isDuplicateEmail){
         emailMessageElement.className = 'input-message error';
         emailMessageElement.textContent = '이미 사용중인 이메일 입니다.';
@@ -124,7 +153,7 @@ function onCheckEmailClickHandler (event){
 function onCheckAuthNumberClickHandler (event) {
     if (!authNumber) return;
 
-    const isEqualAuthNumber = authNumber === AUTH_NUMBER;
+    isEqualAuthNumber = authNumber === AUTH_NUMBER;
     if (!isEqualAuthNumber){
         authNumberMessageElement.className = 'input-message error';
         authNumberMessageElement.textContent = '인증번호가 일치하지 않습니다.';
@@ -154,8 +183,9 @@ function onSignInLinkClickHandler (event) {
 signInLinkElement.addEventListener('click',onSignInLinkClickHandler);
 
 function setSignUpButton (){
+
     const isPrimaryButton = 
-    id && password && passwordCheck && email && authNumber && !isDuplicate && isEmail && !isDuplicateEmail && isEqualAuthNumber;
+    id && password && passwordCheck && email && authNumber && !isDuplicate &&isPasswordPattern &&isEqualPassword && isEmail && !isDuplicateEmail && isEqualAuthNumber;
 
     if (isPrimaryButton) signUpButtonElement.className = 'primary-button full-width';
     else signUpButtonElement.className = 'disable-button full-width';
